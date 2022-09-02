66b2e0f35b681aff5aac930a60e94ac734648df4
jhon <jjhernandezperneth20@gmail.com>
2022-09-01 21:27:56 -0500

    Ejercicio de lista y objeto

---
+++ 02_Ejercicio de lista/index.js
@@ -0,0 +1,13 @@
+const infoPersonal = new Array(4);
+infoPersonal[0] ="Nombre: Jhon_Jader",
+infoPersonal[1]= "Edad "+22,
+infoPersonal[2]= "¿Eres desarrollador?: "+true,
+infoPersonal[3]= "fecha de nacimiento: "+ new Date("01 10 2000"),
+infoPersonal[4]= libro ={
+    titulo: "Emepezando tu dia con Dios",
+    Autor:"Juan Carlos Miguel",
+    Fecha: new Date("08 17 2021"),
+    Url:"https://www.amazon.com/Comenzando-Tu-D%C3%ADa-Dios-Spanish/dp/B09PMHYMZT"
+
+};
+console.log(infoPersonal);
