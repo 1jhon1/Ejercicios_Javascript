af0d036d1786567e6ba1e0a52d91a03c2978d8c0
Uninorte <pernethj@uninorte.edu.co>
2022-09-08 19:35:08 -0500

    Ejercicio numerico

---
+++ 05_Ejercicio de number/number.js
@@ -0,0 +1,13 @@
+let altura_Entera = parseInt(160);
+console.log(altura_Entera);
+let altura_Decimal = parseFloat(1.60);
+console.log(altura_Decimal);
+let peso           = parseFloat(65.2);
+console.log(peso);
+let altura_Decimal_Arriba = altura_Decimal.toFixed();
+console.log(altura_Decimal_Arriba);
+let peso_Abajo = peso.toPrecision(2);
+console.log(peso_Abajo);
+
+let max_Number = ((Number.MAX_VALUE + 1) == Number.MIN_VALUE);
+console.log(max_Number);
